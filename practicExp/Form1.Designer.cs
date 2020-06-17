68ecf573510678c569751b948760e0f941c1f187
Cristian Pop <popcristian9898@yahoo.com>
2020-06-17 20:34:38 +0300

    Add project files.

---
+++ practicExp/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace practicExp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCards = new System.Windows.Forms.DataGridView();
+            this.dgvTransactions = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCards)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCards
+            // 
+            this.dgvCards.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCards.Location = new System.Drawing.Point(0, 36);
+            this.dgvCards.Name = "dgvCards";
+            this.dgvCards.RowHeadersWidth = 51;
+            this.dgvCards.RowTemplate.Height = 24;
+            this.dgvCards.Size = new System.Drawing.Size(788, 150);
+            this.dgvCards.TabIndex = 0;
+            this.dgvCards.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCards_CellContentClick);
+            // 
+            // dgvTransactions
+            // 
+            this.dgvTransactions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransactions.Location = new System.Drawing.Point(0, 221);
+            this.dgvTransactions.Name = "dgvTransactions";
+            this.dgvTransactions.RowHeadersWidth = 51;
+            this.dgvTransactions.RowTemplate.Height = 24;
+            this.dgvTransactions.Size = new System.Drawing.Size(788, 150);
+            this.dgvTransactions.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cards";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Transactions";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(239, 377);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(124, 42);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update db";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvTransactions);
+            this.Controls.Add(this.dgvCards);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCards)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransactions)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCards;
+        private System.Windows.Forms.DataGridView dgvTransactions;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ practicExp/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace practicExp
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection dbConn;
+        SqlDataAdapter daCard, daTransactions;
+        DataSet ds;
+        SqlCommandBuilder cbTransactions;
+        BindingSource bsCard, bsPosts;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvCards_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            this.daTransactions.Update(ds, "Transactions");
+         
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            bsCard = new BindingSource();
+            bsPosts = new BindingSource();
+
+            dgvCards.DataSource = bsCard;
+            dgvTransactions.DataSource = bsPosts;
+
+            string connectionString =
+            "Data Source=(local);Initial Catalog=practicExam;"
+            + "Integrated Security=true";
+            dbConn = new SqlConnection(connectionString);
+
+
+            ds = new DataSet();
+            daCard = new SqlDataAdapter("Select * from Card", dbConn);
+            daTransactions = new SqlDataAdapter("Select * from Transactions", dbConn);
+            cbTransactions = new SqlCommandBuilder(daTransactions);
+           
+            daCard.Fill(ds, "Card");
+            daTransactions.Fill(ds, "Transactions");
+
+            DataRelation dr = new DataRelation("CardTransactions",
+                ds.Tables["Card"].Columns["CardId"],
+                ds.Tables["Transactions"].Columns["CardId"]);
+
+            ds.Relations.Add(dr);
+
+            bsCard.DataSource = ds;
+            bsCard.DataMember = "Card";
+
+            bsPosts.DataSource = bsCard;
+            bsPosts.DataMember = "CardTransactions";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
